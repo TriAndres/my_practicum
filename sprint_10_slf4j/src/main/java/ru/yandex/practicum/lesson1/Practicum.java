1f576e7ea52eb8af153d9b5882ee83b9b636fd40
Andrei <An.S-1@yandex.ru>
2024-09-18 09:25:51 +0300

    sprint_10_slf4j

---
+++ sprint_10_slf4j/src/main/java/ru/yandex/practicum/lesson1/Practicum.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.lesson1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Practicum {
+    private static final Logger log = LoggerFactory.getLogger("Practicum");
+
+    public static void main(String[] args) {
+        System.out.println(log.getName());
+    }
+}
---
+++ sprint_10_slf4j/src/main/java/ru/yandex/practicum/lesson4/Practicum.java
@@ -0,0 +1,6 @@
+package ru.yandex.practicum.lesson4;
+
+public class Practicum {
+
+
+}
